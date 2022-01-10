9c811e5a6e9fbda1d86ff0fb4dd54051d47ff9cb
Nielsen-Mudaki <jumbanielsen@gmail.com>
2022-01-11 01:20:50 +0300

    add toggle feature

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Quota
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.1.1.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.1.1
 
 ## Development server
 
---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote';
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+
+  quotes:Quote[] = [
+    new Quote(1,'Aluta Continua!','Do not stop working to achieve what you want.'),
+    new Quote(2, 'Impossible is nothing', 'You can not be stopped!'),
+    new Quote(3, 'I have a dream', 'Dream on youngblood!!')
+  ];
+
+  toggleDetails(index:number) {
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
